40f958f8a792ebbd3d9820023fd6f395c66ac346
YutakaX17 <kademptr@gmail.com>
2026-06-11 16:19:42 +0200

    Refactor ETL services

---
+++ msr_etl/services/__init__.py
@@ -0,0 +1,2 @@
+from msr_etl.services.base import ETLService
+from msr_etl.services.ubr_service import UBRIndividualService, UBRLocationService
---
+++ msr_etl/sources/__init__.py
@@ -0,0 +1,3 @@
+from msr_etl.sources.base import DataSource
+from msr_etl.sources.exampleIndividualSource import ExampleIndividualSource
+from msr_etl.sources.ubr_source import UBRIndividualSource, UBRLocationSource
