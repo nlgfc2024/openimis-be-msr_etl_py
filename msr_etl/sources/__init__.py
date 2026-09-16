0a45b3b6de19958f408c1be81727a8c0e1793dd6
mcleanka <mcleankasambala@gmail.com>
2026-09-16 22:50:12 +0200

    Export ConfigurableSource from sources package

--- msr_etl/sources/__init__.py
+++ msr_etl/sources/__init__.py
@@ -1,8 +1,10 @@
 from msr_etl.sources.base import DataSource
+from msr_etl.sources.configurable_source import ConfigurableSource
 from msr_etl.sources.ubr_source import UBRIndividualSource, UBRLocationSource
 
 __all__ = [
     "DataSource",
+    "ConfigurableSource",
     "UBRIndividualSource",
     "UBRLocationSource",
 ]
