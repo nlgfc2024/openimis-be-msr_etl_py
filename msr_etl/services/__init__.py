770aeaa53f5e9cc4d3581c64b323465a3732081c
mcleanka <mcleankasambala@gmail.com>
2026-09-16 23:11:46 +0200

    Export configurable services from services package

--- msr_etl/services/__init__.py
+++ msr_etl/services/__init__.py
@@ -1,8 +1,14 @@
 from msr_etl.services.base import MsrETLService
+from msr_etl.services.configurable_service import (
+    ConfigurableIndividualService,
+    ConfigurableLocationService,
+)
 from msr_etl.services.ubr_service import UBRIndividualService, UBRLocationService
 
 __all__ = [
     "MsrETLService",
+    "ConfigurableIndividualService",
+    "ConfigurableLocationService",
     "UBRIndividualService",
     "UBRLocationService",
 ]
